6435eae832568c3b5291bf8de843d9ce5353404b
Elizabeth Manrique <ecmanrique@gmail.com>
2017-12-03 19:34:02 -0500

    Agrego logica js y servidor

---
+++ public/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var forms = document.querySelectorAll("form");
+    var form = forms[0];
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var first_name = document.querySelectorAll("#first_name")[0].value;
+        var last_name = document.querySelectorAll("#last_name")[0].value;
+        
+        var message = document.querySelectorAll("#message")[0];
+        message.innerHTML = message.innerHTML + "<span>" + first_name + " " + last_name + "</span>";
+        
+        var payload = {
+            'first_name': first_name,
+            'last_name': last_name,
+        }
+        
+        $.post("/find", payload);
+      }
+    );
+})
